0c60290d5c5398ff7d23aebc1c83e2728e563e74
kanishkaagrawal2025 <kanishka.agrawal2025@glbajajgroup.org>
2026-07-06 14:01:15 +0530

    Day 13

---
+++ q50.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a[100], sum = 0;
+    float avg;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    avg = (float)sum / n;
+
+    printf("Sum=%d\n", sum);
+    printf("Average=%.2f", avg);
+
+    return 0;
+}
---
+++ q51.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a[100];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int max = a[0], min = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+
+    printf("Largest=%d\n", max);
+    printf("Smallest=%d", min);
+
+    return 0;
+}
