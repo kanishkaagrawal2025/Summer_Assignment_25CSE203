7b5c5d4df14cd0fee084ffff2b772ed11573ccaf
kanishkaagrawal2025 <kanishka.agrawal2025@glbajajgroup.org>
2026-07-08 22:55:30 +0530

    Day 21

---
+++ q82.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i=strlen(s)-1;i>=0;i--)
+        printf("%c",s[i]);
+
+    return 0;
+}
---
+++ q84.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]>='a'&&s[i]<='z')
+            s[i]-=32;
+    }
+
+    printf("%s",s);
+
+    return 0;
+}
